8485ca60ff7978b24dbf81f91248e902c6a1d68a
AnhJays <truongdinhanh07031999@gmail.com>
2022-05-24 09:01:27 +0700

    testing

--- Practice 1/basic/ErrMsgMgmtBC.java
+++ Practice 1/basic/ErrMsgMgmtBC.java
@@ -18,10 +18,9 @@ import com.clt.apps.opus.esm.clv.practicetraining.errmsgmgmt.vo.ErrMsgVO;
 import com.clt.framework.core.layer.event.EventException;
 import com.clt.framework.support.view.signon.SignOnUserAccount;
 
-
 /**
  * ALPS-Practicetraining Business Logic Command Interface<br>
- * - Interface to business logic for ALPS-Practicetraining.<br>
+ * - Interface to business logic for ALPS-Practicetraining hihelotesting<br>
  *
  * @author anhtruong
  * @since J2EE 1.6
@@ -32,18 +31,18 @@ public interface ErrMsgMgmtBC {
 	/**
 	 * [비즈니스대상]을 [행위] 합니다.<br>
 	 * 
-	 * @param ErrMsgVO	errMsgVO
+	 * @param ErrMsgVO errMsgVO
 	 * @return List<ErrMsgVO>
 	 * @exception EventException
 	 */
 	public List<ErrMsgVO> searchErrMsg(ErrMsgVO errMsgVO) throws EventException;
-	
+
 	/**
 	 * [비즈니스대상]을 [행위] 합니다.<br>
 	 * 
 	 * @param ErrMsgVO[] errMsgVO
-	 * @param account SignOnUserAccount
+	 * @param account    SignOnUserAccount
 	 * @exception EventException
 	 */
-	public void manageErrMsg(ErrMsgVO[] errMsgVO,SignOnUserAccount account) throws EventException;
+	public void manageErrMsg(ErrMsgVO[] errMsgVO, SignOnUserAccount account) throws EventException;
 }
